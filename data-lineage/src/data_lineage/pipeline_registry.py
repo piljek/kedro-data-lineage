cd140ce89a782e8c254ffa6a9764f313340fd650
ipiljek <isa.irel@gmail.com>
2023-07-05 22:34:43 +0200

    add mkdocs documentation

--- data-lineage/src/data_lineage/pipeline_registry.py
+++ data-lineage/src/data_lineage/pipeline_registry.py
@@ -13,6 +13,8 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
+
+    
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
     return pipelines
